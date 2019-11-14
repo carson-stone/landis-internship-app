e03341467cb0ca3db89f4c32572840e811d12830
cstone <cstone@cs.odu.edu>
2019-11-13 21:42:28 -0500

    made basic cards for CardsPage

--- landis-app/src/components/Card.js
+++ landis-app/src/components/Card.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
+// name
+// balance
+// credit
+// email
+// phone
+// employer
+// address
+// comments
+// created
+// tags
+// id
+
 function Card() {
-  return <div className='Card'></div>;
+  return (
+    <div className='Card'>
+      <img src='https://i.pravatar.cc/377' alt='user picture' />
+    </div>
+  );
 }
 
 export default Card;
--- landis-app/src/components/CardsPage.js
+++ landis-app/src/components/CardsPage.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 import Card from './Card.js';
-import Indicator from './Card.js';
+import Indicator from './Indicator.js';
 
 function CardsPage() {
-  return <div className='CardsPage'></div>;
+  return (
+    <div className='CardsPage'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 }
 
 export default CardsPage;
