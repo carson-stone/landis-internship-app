6ac5ba5b55e9a8383f409ccfb0ed9f7d28584346
cstone <cstone@cs.odu.edu>
2019-11-14 22:08:44 -0500

    made basic analysis cards

--- landis-app/src/components/AnalysisPage.js
+++ landis-app/src/components/AnalysisPage.js
@@ -1,7 +1,38 @@
 import React, { useState } from 'react';
 
 function AnalysisPage() {
-  return <div className='AnalysisPage'></div>;
+  return (
+    <div className='AnalysisPage'>
+      <div className='AnalysisCard'>
+        <h2>Chart Title</h2>
+        <p>
+          Reprehenderit amet adipisicing nulla ad do veniam voluptate laboris.
+          Deserunt adipisicing sit est Lorem sunt enim aliqua ex. Labore labore
+          excepteur ullamco magna dolor ipsum laboris velit aliqua cupidatat
+          sint incididunt id reprehenderit. Anim aute occaecat velit amet. Non
+          voluptate id aliquip commodo enim laboris aute excepteur mollit ad
+          nulla ex sunt ex.
+        </p>
+      </div>
+      <div className='AnalysisCard'>
+        <h2>Chart Title</h2>
+        <p>
+          Et quis magna consectetur nulla id ad quis consequat irure do cillum
+          nulla cillum. Nisi irure culpa amet sunt id velit anim. Fugiat veniam
+          eiusmod dolore sit irure do cupidatat ullamco aliqua fugiat ex
+          consequat. Non laborum minim cupidatat consequat laborum quis magna
+          ullamco Lorem proident est sit amet.
+        </p>
+      </div>
+      <div className='AnalysisCard'>
+        <h2>Chart Title</h2>
+        <p>
+          Non do velit non dolor. Anim non quis anim nisi. Ullamco aliquip irure
+          ad esse nostrud eiusmod esse eiusmod aliquip ad occaecat irure tempor.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AnalysisPage;
